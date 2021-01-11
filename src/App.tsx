cdc5576703408402d53dbf0e51eb58c1331d8394
Rita Silva <arita-lcas@gmail.com>
2021-01-11 18:17:42 +0000

    Changed icons and color palette

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
   IonTabs
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, square, triangle, homeOutline, addOutline, searchOutline, starOutline, cartOutline } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -50,24 +50,24 @@ const App: React.FC = () => (
           <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
         </IonRouterOutlet>
         <IonTabBar slot="bottom" className="tabBar">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
+          <IonTabButton tab="tab1" href="/tab1" className="menuButton">
+            <IonIcon icon={homeOutline} />
             <IonLabel>Sugestões</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
+          <IonTabButton tab="tab2" href="/tab2" className="menuButton">
+            <IonIcon icon={addOutline} />
             <IonLabel>Adicionar</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
+          <IonTabButton tab="tab3" href="/tab3" className="menuButton">
+            <IonIcon icon={searchOutline} />
             <IonLabel>Pesquisa</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab4" href="/tab4">
-            <IonIcon icon={square} />
+          <IonTabButton tab="tab4" href="/tab4" className="menuButton">
+            <IonIcon icon={starOutline} />
             <IonLabel>Favoritos</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab5" href="/tab5">
-            <IonIcon icon={square} />
+          <IonTabButton tab="tab5" href="/tab5" className="menuButton">
+            <IonIcon icon={cartOutline} />
             <IonLabel>Compras</IonLabel>
           </IonTabButton>
         </IonTabBar>
