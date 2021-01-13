b5049f66667969443e537b6b6fd9c2c2168f562d
Rita Silva <arita-lcas@gmail.com>
2021-01-13 18:49:55 +0000

    Added product images

--- src/components/AlergHomeGrid.tsx
+++ src/components/AlergHomeGrid.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 import { } from '@ionic/react';
 import './AlergHomeGrid.css';
 
-const AlergHomeGrid: React.FC = () => {
+const AlergHomeGrid: React.FC<{ product: { id: number, img: string, name: string, brand: string }[] }> = props => {
     return (
         <div className="homeGridContainer">
-            <div className="homeProducts"></div>
-            <div className="homeProducts"></div>
-            <div className="homeProducts"></div>
-            <div className="homeProducts"></div>
+            <div className="homeProducts">
+                <div className="homeProductImg"><img src={props.product[0].img} /></div>
+            </div>
+            <div className="homeProducts">
+                <div className="homeProductImg"><img src={props.product[1].img} /></div>
+            </div>
+            <div className="homeProducts">
+                <div className="homeProductImg"><img src={props.product[2].img} /></div>
+            </div>
+            <div className="homeProducts">
+                <div className="homeProductImg"><img src={props.product[3].img} /></div>
+            </div>
         </div>
     );
 }
 
 export default AlergHomeGrid;
+
+
+/*<span>{props.product[0].name}</span>*/
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,40 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonSlides, IonSlide } from '@ionic/react';
+import item1 from '../img/item-1.jpg';
+import item2 from '../img/item-2.jpg';
+import item3 from '../img/item-3.jpg';
+import item4 from '../img/item-4.jpg';
 import AlergHeader from '../components/AlergHeader';
 import AlergHomeGrid from '../components/AlergHomeGrid';
 import './Home.css';
+
+
+let homeProducts: { id: number, img: string, name: string, brand: string }[] = [
+  {
+    id: 1,
+    img: item1,
+    name: "Yofu Soja Natural",
+    brand: "Alpro"
+  },
+  {
+    id: 2,
+    img: item2,
+    name: "Chocolate de Leite",
+    brand: "Toblerone"
+  }, 
+  {
+    id: 3,
+    img: item3,
+    name: "Bolachas de Laranja",
+    brand: "Gullón"
+  }, 
+  {
+    id: 4,
+    img: item4,
+    name: "Bolachas Sharkies",
+    brand: "Gullón"
+  }
+];
 
 
 const Home: React.FC = () => {
@@ -20,13 +52,13 @@ const Home: React.FC = () => {
           <div className="homeSlidesContainer">
             <IonSlides pager={true} className="homeSlidesContainer">
               <IonSlide>
-                <AlergHomeGrid></AlergHomeGrid>
+                <AlergHomeGrid product={homeProducts}></AlergHomeGrid>
               </IonSlide>
               <IonSlide>
-                <AlergHomeGrid></AlergHomeGrid>
+                <AlergHomeGrid product={homeProducts}></AlergHomeGrid>
               </IonSlide>
               <IonSlide>
-                <AlergHomeGrid></AlergHomeGrid>
+                <AlergHomeGrid product={homeProducts}></AlergHomeGrid>
               </IonSlide>
             </IonSlides>
           </div>
