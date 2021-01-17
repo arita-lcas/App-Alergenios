9b48a128ea3eefedc761bb7709da898b8200fb59
Rita Silva <arita-lcas@gmail.com>
2021-01-17 17:00:52 +0000

    Continued working on the search page

--- src/components/AlergSearchList.tsx
+++ src/components/AlergSearchList.tsx
@@ -23,15 +23,45 @@ const listArray = [
     "Takeaway"
   ];
 
+  const subTitleArray = [
+    "Leite",
+    "Ovos",
+    "Queijo"
+  ];
+
+  function showSearchDiv () {
+    let element1 = document.getElementById('searchDiv');
+    let element2 = document.getElementById('listCard');
+    let element3 = document.getElementById('listCardTitle');
+  
+    if (element1 && element2 && element3) {
+      element1.classList.toggle('showListContent');
+      element2.classList.toggle('listCardActive');
+      element3.classList.toggle('listCardTitleActive');
+    }
+  
+    return;
+  }
+
 const AlergSearchList: React.FC = () => {
     return (
         <div className="searchList">
           {listArray.map( (listTitle, index) => {
             return (
-            <div className="listCard">
-                <span className="listCardTitle" key={index}>{listTitle}</span>
+            <div>
+              <div className="listCard" id="listCard" onClick={showSearchDiv}>
+                  <span className="listCardTitle" id="listCardTitle" key={index}>{listTitle}</span>
+              </div>
+              <div className="hideListContent" id="searchDiv">
+                {subTitleArray.map( (listSubTitle, index) => {
+                  return (
+                    <span key={index}>{listSubTitle}</span>
+                  )
+                } )}
+              </div>
             </div>
-          )} )}
+            )
+          } )}
         </div>
     );
 };
--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -5,6 +5,17 @@ import AlergHeader from '../components/AlergHeader';
 import { personCircleOutline } from 'ionicons/icons';
 import './Favorites.css';
 
+
+function showSearchDiv () {
+  let element = document.getElementById('searchDiv');
+
+  if (element) {
+    element.classList.toggle('showcontent');
+  }
+
+  return;
+}
+
 const Favorites: React.FC = () => {
   return (
     <IonPage>
@@ -15,12 +26,10 @@ const Favorites: React.FC = () => {
           headerRight={"headerUser"} />
       </IonHeader>
       <IonContent fullscreen className="content">
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 4 page" />
+        {/* <button type="button" className="collapsible" onClick={showSearchDiv}>Open Collapsible</button>
+        <div className="hidecontent" id="searchDiv">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div> */}
       </IonContent>
     </IonPage>
   );
