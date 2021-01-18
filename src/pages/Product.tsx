569f941bb1754f23e3b742a370f2861c22022b6b
Rita Silva <arita-lcas@gmail.com>
2021-01-18 16:18:34 +0000

    Started styling the product page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonIcon } from '@ionic/react';
+import { chevronDownOutline, informationCircle } from 'ionicons/icons';
 import AlergHeader from '../components/AlergHeader';
+import './Product.css';
 
 
 const Product: React.FC = () => {
@@ -10,10 +12,28 @@ const Product: React.FC = () => {
                 <AlergHeader 
                     headerLeft={"headerReturn"} 
                     headerTitle={""} 
-                    headerRight={""} />
+                    headerRight={"headerEdit"} />
             </IonHeader>
             <IonContent className="content">
-
+                <div className="productPWrapper">
+                    <div className="productPInfo">
+                        <h1 className="alergTitle">Título</h1>
+                        <span>Marca</span>
+                        <div className="productPImg"></div>
+                    </div>
+                    <div className="productProfile1 productProfiles">
+                        <span>Marta</span>
+                        <IonIcon icon={chevronDownOutline} />
+                    </div>
+                    <div className="productProfile2 productProfiles">
+                        <span>Sofia</span>
+                        <IonIcon icon={chevronDownOutline} />
+                    </div>
+                    <div className="productPIngredients">
+                        <IonIcon icon={informationCircle} className="ingredientsIcon" />
+                        <span>Ver ingredientes</span>
+                    </div>
+                </div>
             </IonContent>
         </IonPage>
     );
